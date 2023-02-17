93994cd2b24d52edd8f5feefe26085485b63c4a2
Oz Rhodes <44510679+OzRhodes@users.noreply.github.com>
2023-02-17 18:57:46 +0000

    Binary Breadth Search first upload

---
+++ sort_binary_list_levels.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+        
+        
+def tree_by_levels(node):
+    result = []
+    if node != None:
+    	
+	    queue = deque()
+	    queue.append(node)
+	    #while node != None:
+	    while len(queue)>0:
+		    q_node = queue.popleft()
+		    if q_node.left != None:
+		    	queue.append(q_node.left)
+		    if q_node.right != None:
+		    	queue.append(q_node.right)
+		    result.append(q_node.value)
+
+    return result
+
+
+#print(tree_by_levels((None,None,4)))
+#print(tree_by_levels(Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1)))
+
+
