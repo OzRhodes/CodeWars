afdeca517ca8c7ca66b5e800d642c9b294474e3a
oz rhodes <oz@ozrhodes.com>
2023-02-23 09:56:49 +0000

    started the parsing

--- parse_int.py
+++ parse_int.py
@@ -1,27 +1,41 @@
 def parse_int(string):
     # split the string on million, thousand, hundredto get a list of segmented values
-    value = string.split('million','thousand','hundred')
+    value = string.split()
     digits = {
         'one':1,
         'two':2,
-        'three',3,
-        'four',4
-        
-        
-        
-        
+        'three':3,
+        'four':4,
+        'five':5,
+        'six':6,
+        'seven':7,
+        'eight':8,
+        'nine':9,
+        'zero':0
         }
     teens = {
 
     }
     tens = {
+        'ten':10,
+        'twenty':20,
+        'thirty':30
 
     }
-    return # number
+    # address the dash
+
+    number = 0
+    
+    for i in range (len(value)):
+        number +=digits[value[i]]
+
+        
+
+    return number
 
 
 input="one"
-print(parse_int())
+print(parse_int(input))
 
 '''
 test.assert_equals(parse_int('one'), 1)
